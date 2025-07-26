043a214530bb11d7852799234559b0380995ecc8
Kutakula Sasank <kutakula.sasank2022@vitstudent.ac.in>
2025-07-26 22:01:52 +0530

    prev1234

--- src/COMPONENTS/Selectadd.jsx
+++ src/COMPONENTS/Selectadd.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const Selectadd = () => {
   const [opt, setopt] = useState("");
-  const options = ["DSA", "OS", "CN", "APPTITUDE", "SQL", "CLOUD"];
+  const options = ["DSA", "OS", "CN", "APPTITUDE", "SQL", "CLOUD", "REACT"];
   const navigate = useNavigate();
 
   const handleoptions = (ind) => {
